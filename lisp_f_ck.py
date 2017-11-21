9a14f9503c3a866ef5d03909956dcea595548875
fabiolamfleury <fabiola.mfleury@gmail.com>
2017-11-21 17:48:07 -0200

    Updating parser rules

--- lisp_f_ck.py
+++ lisp_f_ck.py
@@ -1,5 +1,6 @@
 import ox
 import click
+import pprint
 
 lexer = ox.make_lexer([
     ('RIGHT', r'right'), # > in brainfuck
@@ -16,52 +17,54 @@ lexer = ox.make_lexer([
     ('NUMBER', r'\d+'),
     ('PARENTESE_A', r'\('),
     ('PARENTESE_F', r'\)'),
+    ('NAME', r'[-a-zA-Z]+'),
     ('COMMENT', r';.*'),
     ('NEWLINE', r'\n'),
     ('SPACE', r'\s+')
 ])
 
+
 tokens = ['RIGHT', 'LEFT', 'INC', 'DEC', 'SUB', 'ADD', 'NUMBER','PRINT', 'LOOP',
-            'READ','DEF','COMMENT','PARENTESE_F','PARENTESE_A','DO']
+            'READ','DEF','PARENTESE_F','PARENTESE_A','DO','NAME']
 
-operator = lambda type_op: ('operator', type_op)
-op = lambda op, x: (op, x)
+operator = lambda type_op: (type_op)
+op = lambda op: (op)
+opr = lambda op, num: (op, num)
 
-#expr = lambda x, y: (x, y)
 parser = ox.make_parser([
-    #('program : PARENTESE_A DO expr expr PARENTESE_F ',)
-    #('program : PARENTESE_A DO expr expr PARENTESE_F ', lambda a,b,c,d,e: (b,c,d)),
-    ('program : PARENTESE_A DO expr  PARENTESE_F', lambda x,y,z,w: (y,z)),
-    ('expr : expr expr', lambda x,y: (x,y)),
-    ('expr : expr', lambda x: x),
-    ('expr : PARENTESE_A expr PARENTESE_F', lambda x,y,z: y),
-    ('expr : PARENTESE_A operator PARENTESE_F', lambda x,y,z: y),
-    ('expr : expr operator', lambda x,y: (x,y)),
-    ('expr : operator', lambda x: x),
+	('program : PARENTESE_A expr PARENTESE_F', lambda x,y,z: y),
+    ('program : PARENTESE_A PARENTESE_F', lambda x,y: '()'),
+	('expr : operator expr', lambda x,y: (x,) + y),
+	('expr : operator', lambda x: (x,)),
+	('operator : program', op),
     ('operator : LOOP', operator),
+    ('operator : DO', operator),
     ('operator : RIGHT', operator),
     ('operator : LEFT', operator),
     ('operator : READ', operator),
     ('operator : INC', operator),
     ('operator : DEC', operator),
+    ('operator : DEF', operator),
     ('operator : PRINT', operator),
-    ('operator : ADD NUMBER', op),
-    ('operator : SUB NUMBER', op),
+    ('operator : ADD', operator),
+    ('operator : SUB', operator),
+    ('operator : NAME', operator),
+    ('operator : NUMBER', operator),
 ], tokens)
+
 
 @click.command()
 @click.argument('source', type=click.File('r'))
 def make_tree(source):
     program = source.read()
-    print(program)
+    print('program: ', program)
     tokens = lexer(program)
-    #print('tokens:', tokens)
 
     # removing space and comment tokens before passing list to parser
     parser_tokens = [token for token in tokens if token.type != 'COMMENT' and token.type != 'SPACE']
-    #print(parser_tokens)
+
     tree = parser(parser_tokens)
-    print('tree:', tree) # Abstract syntax tree
+    print('\n\ntree:', tree) # Abstract syntax tree
 
 if __name__ == '__main__':
     make_tree()
